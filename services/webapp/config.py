440618b845dacb64d425ee8a50a3bd55e5bd572a
Andres Higuera <114029522+Makhai412@users.noreply.github.com>
2025-05-26 21:48:37 -0500

    Update config.py

--- services/webapp/config.py
+++ services/webapp/config.py
@@ -1,12 +1,8 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'myflaskapp')
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'myflaskapp')
+    SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
